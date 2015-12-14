dc75238e1cddefe8c9888f6ec193895814af31e2
Harry Chen <harry@harry.gg>
2015-12-15 01:03:32 +0800

    feat(ux): Add UX for login and signup

--- app/assets/javascripts/angular/controllers/static_pages.js
+++ app/assets/javascripts/angular/controllers/static_pages.js
@@ -1,7 +1,15 @@
-app.controller('StaticPagesCtrl', ['$scope', '$location', function($scope, $location){
+app.controller('StaticPagesCtrl', ['$scope', '$location', '$auth', function($scope, $location, $auth){
   $scope.message = "Hello World!";
+  $scope.currentUser = null;
 
   $scope.signUp = function(){
     $location.path('signup');
   };
+
+  $auth.validateUser().then(function(resp){
+    console.log(resp);
+    $scope.currentUser = resp;
+  }).catch(function(resp){
+    console.log(resp);
+  });
 }]);
